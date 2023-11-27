0df5e766242eeec63c2986b10053ba142410792a
ETheoParis <e.theo.paris@gmail.com>
2023-11-27 11:36:49 +0100

    exit added

--- src/enseash.c
+++ src/enseash.c
@@ -5,28 +5,37 @@
 #include <string.h>
 #include <stdio.h>
 #include <sys/wait.h>
+#include <stdlib.h>
 
 
 int main(int argc, char* argv[]){
 	
 	char input[SIZE];
 	int status;
+	int pidoriginal = getpid();
+	int pid=pidoriginal;
+
 	while(1){
-		if(write(STDOUT_FILENO,MESSAGE,strlen(MESSAGE))==-1){perror("write");}
-		int pid = fork();
-		if(pid<0){perror("fork impossible");}
-		if(pid == CHILDPID){
+		if(write(STDOUT_FILENO,MESSAGE,strlen(MESSAGE))==-1)	{perror("write enseash");}
+		if(pid<0)	{perror("fork impossible");}
+		if(pid > 0){
 			if(read(STDIN_FILENO,input,SIZE)==-1){perror("read");}
-		
-	
 			input[strcspn(input, "\n")] = '\0';
-	
-			if(execl(input,input,NULL)==-1){
-				if(execlp(input,input,NULL)==-1){perror("wrong command");}
-			}
+		
+			if(strcmp(input,"exit")==0){
+				if(write(STDOUT_FILENO,"bye bye \n",10)==-1)	{perror("bye bye error");}
+			exit(EXIT_SUCCESS);
 			
-		}
-		if(pid > 0){wait(&status);}
+			}
+			if(getpid() == pidoriginal){pid = fork();}
+			if(pid == CHILDPID){
+				if(execl(input,input,NULL)==-1){
+					if(execlp(input,input,NULL)==-1)	{perror("wrong command");}
+				}
+				exit(EXIT_SUCCESS);
+			}
+			wait(&status);
 	}
 	
+	}
 }
