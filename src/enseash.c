ddca52ba7122c4189ba65d91da63d2c925c1cfcc
ETheoParis <e.theo.paris@gmail.com>
2023-11-27 10:44:49 +0100

    modified enseash

--- src/enseash.c
+++ src/enseash.c
@@ -1,19 +1,32 @@
 #define SIZE 1024
-#define MESSAGE "hello world\n"
+#define MESSAGE "enseash % "
+#define CHILDPID 0
 #include <unistd.h>
 #include <string.h>
 #include <stdio.h>
+#include <sys/wait.h>
 
 
 int main(int argc, char* argv[]){
-	write(STDOUT_FILENO,MESSAGE,strlen(MESSAGE));
 	
 	char input[SIZE];
-	read(STDIN_FILENO,input,SIZE);
+	int status;
+	while(1){
+		if(write(STDOUT_FILENO,MESSAGE,strlen(MESSAGE))==-1){perror("write");}
+		int pid = fork();
+		if(pid<0){perror("fork impossible");}
+		if(pid == CHILDPID){
+			if(read(STDIN_FILENO,input,SIZE)==-1){perror("read");}
+		
 	
-	input[strcspn(input, "\n")] = '\0';
+			input[strcspn(input, "\n")] = '\0';
 	
-	if(execl(input,input,NULL)==-1){
-		execlp(input,input,NULL);}
-
+			if(execl(input,input,NULL)==-1){
+				if(execlp(input,input,NULL)==-1){perror("wrong command");}
+			}
+			
+		}
+		if(pid > 0){wait(&status);}
+	}
+	
 }
